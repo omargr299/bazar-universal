95b9cbec88f0d65d143006c2ae336cd097676af3
Omar <omar.gzz.rdz@hotmail.com>
2023-11-02 19:17:59 -0600

    add SEO tags and other changes

--- src/app/api/items/[id]/route.tsx
+++ src/app/api/items/[id]/route.tsx
@@ -1,6 +1,7 @@
 import { getProduct } from "@/services/products"
 import { NextResponse } from "next/server"
 
+
 export async function GET(request: Request, { params: { id } }: { params: { id: string } }) {
     console.log(id)
 
--- src/app/items/[id]/page.tsx
+++ src/app/items/[id]/page.tsx
@@ -1,5 +1,7 @@
 import './styles.css'
+import Head from 'next/head'
 import { Slider } from "@/app/components/slider"
+import { Product } from '@/types'
 
 
 
@@ -7,14 +9,19 @@ export default async function Item({ params: { id } }: { params: { id: string } 
 
     const res = await fetch(`https://${process.env.HOST_API}/api/items/${id}`, { method: 'GET' })
 
-    const product = (await res.json())
+    const product: Product | undefined = (await res.json())
 
     return (
         <>
             {
                 product?.id ?
                     <main className="[&>*]:my-3">
-
+                        <Head>
+                            <meta property="og:type" content="article" />
+                            <meta property="og:title" content={product.title} />
+                            <meta property="og:description" content={product.description} />
+                            <meta property="og:image" content={product.thumbnail} />
+                        </Head>
                         <Slider images={product.images} title={product.title} />
 
                         <h2>{product.title}</h2>
